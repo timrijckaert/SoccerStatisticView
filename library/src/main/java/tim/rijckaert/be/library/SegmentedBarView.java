3c77fba946cb2dc9cadc531037dfb32073703773
Tim Rijckaert <tim.rijckaert@vrt.be>
2016-06-09 01:09:30 +0200

    Well it's a start

---
+++ library/src/main/java/tim/rijckaert/be/library/SegmentedBarView.java
@@ -0,0 +1,292 @@
+package tim.rijckaert.be.library;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+/**
+ * Created by tim on 08.06.16.
+ */
+public class SegmentedBarView extends View {
+
+    //<editor-fold desc="Constants">
+    private static final boolean DEFAULT_ABSOLUTE = false;
+    private static final int DEFAULT_VALUE = 0;
+    private static final boolean DEFAULT_IS_ANIMATED = false;
+
+    private static final int FINE_BAR_HEIGHT = 3;
+    private static final int BAR_HEIGHT = 8;
+    private final int SPACE_BETWEEN_GUIDES = 15;
+    //</editor-fold>
+
+    //<editor-fold desc="Attributes">
+    private final int homeTeamColor;
+    private final int awayTeamColor;
+    private final int indifferentColor;
+    private final int fineLineColor;
+    private final Stat stat;
+    private final boolean isAnimated;
+    //</editor-fold>
+
+    //<editor-fold desc="Paint">
+    private Paint fineLinePaint;
+    private Paint homeRectPaint;
+    private Paint awayRectPaint;
+    private Paint indifferentPaint;
+    //</editor-fold>
+
+    //<editor-fold desc="Chaining Constructors">
+    public SegmentedBarView(final Context context) {
+        this(context, null);
+    }
+
+    public SegmentedBarView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+
+        final TypedArray obtainStyledAttributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SegmentedBarView, 0, 0);
+        this.homeTeamColor = obtainStyledAttributes.getColor(R.styleable.SegmentedBarView_colorHomeTeam, Color.BLACK);
+        this.awayTeamColor = obtainStyledAttributes.getColor(R.styleable.SegmentedBarView_colorAwayTeam, Color.GRAY);
+        this.indifferentColor = obtainStyledAttributes.getColor(R.styleable.SegmentedBarView_colorIndifferent, Color.GRAY);
+        this.fineLineColor = obtainStyledAttributes.getColor(R.styleable.SegmentedBarView_colorFineLine, Color.GRAY);
+        this.isAnimated = obtainStyledAttributes.getBoolean(R.styleable.SegmentedBarView_animated, DEFAULT_IS_ANIMATED);
+
+        final String label = obtainStyledAttributes.getString(R.styleable.SegmentedBarView_label);
+        final boolean isPercentageValue = obtainStyledAttributes.getBoolean(R.styleable.SegmentedBarView_isPercentageValue, DEFAULT_ABSOLUTE);
+        final int homeTeamValue = obtainStyledAttributes.getInteger(R.styleable.SegmentedBarView_homeTeamValue, DEFAULT_VALUE);
+        final int awayTeamValue = obtainStyledAttributes.getInteger(R.styleable.SegmentedBarView_awayTeamValue, DEFAULT_VALUE);
+        this.stat = new Stat(label, homeTeamValue, awayTeamValue, isPercentageValue ? Unit.PERCENTAGE : Unit.ABSOLUTE);
+
+        obtainStyledAttributes.recycle();
+        initViews();
+    }
+    //</editor-fold>
+
+    private void initViews() {
+        checkPreconditionsAreSet();
+
+        fineLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        fineLinePaint.setColor(fineLineColor);
+
+        indifferentPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        indifferentPaint.setColor(indifferentColor);
+
+        homeRectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        homeRectPaint.setColor(homeTeamColor);
+
+        awayRectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        awayRectPaint.setColor(awayTeamColor);
+    }
+
+    private void checkPreconditionsAreSet() {
+        if (stat == null) {
+            throw new RuntimeException("Error! Stat object could not be instantiated. Did you fill the label?");
+        }
+
+        if (stat.getLabel() == null) {
+            throw new RuntimeException("Error! No label was set!");
+        }
+
+        if (stat.getHomeTeamValue() < 0 || stat.getAwayTeamValue() < 0) {
+            throw new RuntimeException("Error! the value should be bigger than 0");
+        }
+
+        if (stat.getUnit().equals(Unit.PERCENTAGE)) {
+            if (stat.getSum() != 100) {
+                throw new RuntimeException("Error! the home team value and away team value should add up to 100% if you are using the percentage unit");
+            }
+        }
+    }
+
+    @Override
+    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        setMeasuredDimension(widthMeasureSpec, measureHeight(heightMeasureSpec));
+    }
+
+    private int measureHeight(int measureSpec) {
+        int size = getPaddingTop() + getPaddingBottom();
+        size += getSizeInPixels(TypedValue.COMPLEX_UNIT_DIP, BAR_HEIGHT);
+        return resolveSizeAndState(size, measureSpec, 0);
+    }
+
+    private float getSizeInPixels(final int typedValueType, final int typedValue) {
+        return TypedValue.applyDimension(typedValueType, typedValue, getResources().getDisplayMetrics());
+    }
+
+    @Override
+    protected void onDraw(final Canvas canvas) {
+        final int completeWidth = getWidth() - getPaddingLeft() - getPaddingRight();
+        final int halfCompleteWidth = completeWidth / 2 - SPACE_BETWEEN_GUIDES;
+
+        drawGuideLines(canvas, halfCompleteWidth);
+
+        final RectF homeRectF = getRectF(true, halfCompleteWidth);
+        final RectF awayRectF = getRectF(false, halfCompleteWidth);
+        drawSegmentedBarsValues(canvas, homeRectF, awayRectF);
+    }
+
+    private void drawSegmentedBarsValues(final Canvas canvas, final RectF homeRectF, final RectF awayRectF) {
+        if (homeRectF != null) {
+            canvas.drawRect(homeRectF, getAppropriatePaint(true));
+        }
+
+        if (awayRectF != null) {
+            canvas.drawRect(awayRectF, getAppropriatePaint(false));
+        }
+    }
+
+    private void drawGuideLines(final Canvas canvas, final int halfCompleteWidth) {
+        final float barCenter = getBarCenter();
+        final float halfBarHeight = getSizeInPixels(TypedValue.COMPLEX_UNIT_DIP, FINE_BAR_HEIGHT) / 2;
+
+        final RectF leftRect = new RectF(
+                getPaddingLeft(),
+                barCenter - halfBarHeight,
+                halfCompleteWidth - (SPACE_BETWEEN_GUIDES / 2),
+                barCenter + halfBarHeight
+        );
+
+        final RectF rightRect = new RectF(
+                getPaddingLeft() + halfCompleteWidth + (SPACE_BETWEEN_GUIDES / 2),
+                barCenter - halfBarHeight,
+                getWidth() - getPaddingRight(),
+                barCenter + halfBarHeight
+        );
+
+        canvas.drawRect(leftRect, fineLinePaint);
+        canvas.drawRect(rightRect, fineLinePaint);
+    }
+
+    //<editor-fold desc="Helper Methods">
+    private float getBarCenter() {
+        return getHeight() - getPaddingTop() - getPaddingBottom() - (getSizeInPixels(TypedValue.COMPLEX_UNIT_DIP, BAR_HEIGHT)) / 2;
+    }
+
+    private Paint getAppropriatePaint(final boolean isHomeTeam) {
+        Paint appropriatePaint = isHomeTeam ? homeRectPaint : awayRectPaint;
+        if (stat.getHomeTeamValue() == stat.getAwayTeamValue()) {
+            appropriatePaint = indifferentPaint;
+        }
+        return appropriatePaint;
+    }
+
+    private RectF getRectF(final boolean isHomeTeam, final int halfCompleteWidth) {
+        RectF rect = null;
+
+        final int top = 0;
+        final int bottom = getHeight() - getPaddingBottom() - getPaddingTop();
+
+        final double valueBarWidth = getValueBarWidth(isHomeTeam, halfCompleteWidth);
+        if (valueBarWidth != 0) {
+            final double left = getLeft(isHomeTeam, halfCompleteWidth, valueBarWidth);
+            final double right = getRight(isHomeTeam, halfCompleteWidth, valueBarWidth);
+
+            rect = new RectF((float) left, top, (float) right, bottom);
+        }
+        return rect;
+    }
+
+    private double getLeft(final boolean isHomeTeam, final int halfCompleteWidth, final double valueBarWidth) {
+        double left;
+        if (isHomeTeam) {
+            left = halfCompleteWidth - (SPACE_BETWEEN_GUIDES / 2) - valueBarWidth;
+        } else {
+            left = halfCompleteWidth + (SPACE_BETWEEN_GUIDES / 2);
+        }
+        return left;
+    }
+
+    private double getRight(final boolean isHomeTeam, final int halfCompleteWidth, final double valueBarWidth) {
+        double right;
+        if (isHomeTeam) {
+            right = halfCompleteWidth - (SPACE_BETWEEN_GUIDES / 2);
+        } else {
+            right = halfCompleteWidth + (SPACE_BETWEEN_GUIDES / 2) + valueBarWidth;
+        }
+        return right;
+    }
+
+    private double getValueBarWidth(final boolean isHomeTeam, final int halfCompleteWidth) {
+        double valueBarWidth = 0;
+        final double factor = getFactor(isHomeTeam);
+        if (factor != 0) {
+            valueBarWidth = halfCompleteWidth / factor;
+        }
+        return valueBarWidth;
+    }
+
+    private double getFactor(final boolean isHomeTeam) {
+        double factor = 1.;
+        switch (stat.getUnit()) {
+            case PERCENTAGE:
+                factor = isHomeTeam ?
+                        100d / stat.getHomeTeamValue() :
+                        100d / stat.getAwayTeamValue();
+                break;
+            case ABSOLUTE:
+                final double sumOfValues = (double) stat.getSum();
+                factor = isHomeTeam ?
+                        sumOfValues / stat.getHomeTeamValue() :
+                        sumOfValues / stat.getAwayTeamValue();
+                break;
+        }
+        return factor;
+    }
+    //</editor-fold>
+
+    enum Unit {
+        PERCENTAGE("%"),
+        ABSOLUTE("");
+
+        private final String extension;
+
+        Unit(final String extension) {
+            this.extension = extension;
+        }
+
+        private String getExtension() {
+            return extension;
+        }
+    }
+
+    public class Stat {
+        private final int sum;
+        private String label;
+        private int homeTeamValue;
+        private int awayTeamValue;
+        private Unit unit;
+
+        private Stat(final String label, final int homeTeamValue, final int awayTeamValue, final Unit unit) {
+            this.label = label;
+            this.homeTeamValue = homeTeamValue;
+            this.awayTeamValue = awayTeamValue;
+            this.sum = homeTeamValue + awayTeamValue;
+            this.unit = unit;
+        }
+
+        private String getLabel() {
+            return label;
+        }
+
+        private int getHomeTeamValue() {
+            return homeTeamValue;
+        }
+
+        private int getAwayTeamValue() {
+            return awayTeamValue;
+        }
+
+        private Unit getUnit() {
+            return unit;
+        }
+
+        private int getSum() {
+            return sum;
+        }
+    }
+}
--- library/src/main/java/tim/rijckaert/be/library/StatisticsView.java
+++ library/src/main/java/tim/rijckaert/be/library/StatisticsView.java
@@ -1,169 +1,46 @@
 package tim.rijckaert.be.library;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Paint.Align;
-import android.support.annotation.ColorInt;
 import android.util.AttributeSet;
-import android.util.TypedValue;
-import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
  * Created by tim on 08.06.16.
  */
-public class StatisticsView extends ViewGroup {
+public class StatisticsView extends LinearLayout {
 
-    public static final String TAG = "StatisticsView";
-    private static final int DEFAULT_TEXT_SIZE = 12;
+    private TextView homeTeamTextView;
+    private TextView awayTeamTextView;
+    private TextView labelTextView;
+    private SegmentedBarView segmentedBarView;
 
-    //<editor-fold desc="Attributes">
-    private final int homeTeamColor;
-    private final int awayTeamColor;
-    private final int indifferentColor;
-    private final int fineLineColor;
-    private final boolean isAnimated;
-    //</editor-fold>
-
-    //<editor-fold desc="Paint">
-    private Paint valuePaint;
-    private Paint fineLinePaint;
-    private Paint homeValuePaint;
-    private Paint awayValuePaint;
-    private Paint labelPaint;
-    //</editor-fold>
-
-    //<editor-fold desc="Chaining Constructors">
     public StatisticsView(final Context context) {
         this(context, null);
     }
 
     public StatisticsView(final Context context, final AttributeSet attrs) {
-        this(context, attrs, 0);
+        super(context, attrs);
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.compound_statistics_labels, this, true);
     }
 
-    public StatisticsView(final Context context,
-                          @ColorInt final int colorHomeTeam,
-                          @ColorInt final int awayTeamColor,
-                          @ColorInt final int indifferentColor,
-                          @ColorInt final int fineLineColor,
-                          final boolean isAnimated) {
-        super(context);
-        this.homeTeamColor = colorHomeTeam;
-        this.awayTeamColor = awayTeamColor;
-        this.indifferentColor = indifferentColor;
-        this.fineLineColor = fineLineColor;
-        this.isAnimated = isAnimated;
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        homeTeamTextView = (TextView) findViewById(R.id.statisticsView_homeTeam_tv);
+        awayTeamTextView = (TextView) findViewById(R.id.statisticsView_awayTeam_tv);
+        labelTextView = (TextView) findViewById(R.id.statisticsView_label_tv);
+        segmentedBarView = (SegmentedBarView) findViewById(R.id.statisticsView_segmentedBarView);
 
         initViews();
     }
-
-    public StatisticsView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-
-        final TypedArray obtainStyledAttributes = context.getTheme().obtainStyledAttributes(R.styleable.StatisticsView);
-        this.homeTeamColor = obtainStyledAttributes.getColor(R.styleable.StatisticsView_colorHomeTeam, Color.BLACK);
-        this.awayTeamColor = obtainStyledAttributes.getColor(R.styleable.StatisticsView_colorAwayTeam, Color.GRAY);
-        this.indifferentColor = obtainStyledAttributes.getColor(R.styleable.StatisticsView_colorIndifferent, Color.GRAY);
-        this.fineLineColor = obtainStyledAttributes.getColor(R.styleable.StatisticsView_colorFineLine, Color.GRAY);
-        this.isAnimated = obtainStyledAttributes.getBoolean(R.styleable.StatisticsView_animated, true);
-        obtainStyledAttributes.recycle();
-
-        initViews();
-    }
-    //</editor-fold>
 
     private void initViews() {
-        valuePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        valuePaint.setTextSize(getSizeFromPx(TypedValue.COMPLEX_UNIT_SP, DEFAULT_TEXT_SIZE));
-        valuePaint.setColor(Color.BLACK);
-        valuePaint.setTextAlign(Paint.Align.LEFT);
-
-        labelPaint = valuePaint;
-        labelPaint.setTextAlign(Align.CENTER);
-
-        fineLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        fineLinePaint.setColor(fineLineColor);
-
-        homeValuePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        homeValuePaint.setColor(homeTeamColor);
-
-        awayValuePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        awayValuePaint.setColor(awayTeamColor);
+        homeTeamTextView.setText("95%");
+        awayTeamTextView.setText("5%");
+        labelTextView.setText("Balbezit");
     }
-
-    private float getSizeFromPx(final int typedValue, final int pixelSize) {
-        return TypedValue.applyDimension(typedValue, pixelSize, getResources().getDisplayMetrics());
-    }
-
-    @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        // TODO: 08.06.16 start animation
-    }
-
-    @Override
-    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    }
-
-    @Override
-    protected void onLayout(final boolean changed, final int left, final int top, final int right, final int bottom) {
-
-        final TextView child = new TextView(getContext());
-        child.setText("This is jut a test!");
-        addViewInLayout(child, 0, new ViewGroup.LayoutParams(100, 100));
-
-        invalidate();
-        requestLayout();
-    }
-
-    @Override
-    protected void onDraw(final Canvas canvas) {
-        super.onDraw(canvas);
-    }
-
-    @Override
-    protected void onDetachedFromWindow() {
-        super.onDetachedFromWindow();
-        // TODO: 08.06.16 stop animations
-    }
-
-    //<editor-fold desc="LayoutParams">
-    public static class LayoutParams extends ViewGroup.LayoutParams {
-
-        private final int width;
-        private final int height;
-
-        public LayoutParams(final int width, final int height) {
-            super(width, height);
-            this.width = width;
-            this.height = height;
-        }
-
-        private int getWidth() {
-            return width;
-        }
-
-        private int getHeight() {
-            return height;
-        }
-    }
-    //</editor-fold>
-
-    //<editor-fold desc="Saved Instance">
-//    static class SavedInstanceState extends BaseSavedState {
-//
-//        public SavedInstanceState(final Parcel source) {
-//            super(source);
-//        }
-//
-//        public SavedInstanceState(final Parcelable superState) {
-//            super(superState);
-//        }
-//    }
-    //</editor-fold>
 }
