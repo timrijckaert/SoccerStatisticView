67b07fc12de287c00cfd547409e5402951483053
Joshua Achoka <joshua.nabongo@equitybank.co.ke>
2019-07-16 10:47:57 +0300

    rename library to soccerstatisticsview
    converted to androidx

--- soccerstatisticsview/src/main/java/tim/rijckaert/be/library/SegmentedBarView.java
+++ soccerstatisticsview/src/main/java/tim/rijckaert/be/library/SegmentedBarView.java
@@ -7,7 +7,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
